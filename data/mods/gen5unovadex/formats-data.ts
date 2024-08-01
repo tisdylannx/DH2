8543e01b39aa99d214c07e0252b1d7e30189039e
tisdylannx <itsdylannx@gmail.com>
2024-08-01 12:53:12 -0400

    Create formats-data.ts

---
+++ data/mods/gen5unovadex/formats-data.ts
@@ -0,0 +1,502 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	victini: {
+		tier: "OU",
+	},
+	snivy: {
+		tier: "LC",
+	},
+	servine: {
+		tier: "NFE",
+	},
+	serperior: {
+		tier: "OU",
+	},
+	tepig: {
+		tier: "LC",
+	},
+	pignite: {
+		tier: "NFE",
+	},
+	emboar: {
+		tier: "OU",
+	},
+	oshawott: {
+		tier: "LC",
+	},
+	dewott: {
+		tier: "NFE",
+	},
+	samurott: {
+		tier: "OU",
+	},
+	patrat: {
+		tier: "LC",
+	},
+	watchog: {
+		tier: "OU",
+	},
+	lillipup: {
+		tier: "LC",
+	},
+	herdier: {
+		tier: "NFE",
+	},
+	stoutland: {
+		tier: "OU",
+	},
+	purrloin: {
+		tier: "LC",
+	},
+	liepard: {
+		tier: "OU",
+	},
+	pansage: {
+		tier: "LC",
+	},
+	simisage: {
+		tier: "OU",
+	},
+	pansear: {
+		tier: "LC",
+	},
+	simisear: {
+		tier: "OU",
+	},
+	panpour: {
+		tier: "LC",
+	},
+	simipour: {
+		tier: "OU",
+	},
+	munna: {
+		tier: "LC",
+	},
+	musharna: {
+		tier: "OU",
+	},
+	pidove: {
+		tier: "LC",
+	},
+	tranquill: {
+		tier: "NFE",
+	},
+	unfezant: {
+		tier: "OU",
+	},
+	blitzle: {
+		tier: "LC",
+	},
+	zebstrika: {
+		tier: "OU",
+	},
+	roggenrola: {
+		tier: "LC",
+	},
+	boldore: {
+		tier: "NFE",
+	},
+	gigalith: {
+		tier: "OU",
+	},
+	woobat: {
+		tier: "LC",
+	},
+	swoobat: {
+		tier: "OU",
+	},
+	drilbur: {
+		tier: "LC",
+	},
+	excadrill: {
+		tier: "OU",
+	},
+	audino: {
+		tier: "OU",
+	},
+	timburr: {
+		tier: "LC",
+	},
+	gurdurr: {
+		tier: "NFE",
+	},
+	conkeldurr: {
+		tier: "OU",
+	},
+	tympole: {
+		tier: "LC",
+	},
+	palpitoad: {
+		tier: "NFE",
+	},
+	seismitoad: {
+		tier: "OU",
+	},
+	throh: {
+		tier: "OU",
+	},
+	sawk: {
+		tier: "OU",
+	},
+	sewaddle: {
+		tier: "LC",
+	},
+	swadloon: {
+		tier: "NFE",
+	},
+	leavanny: {
+		tier: "OU",
+	},
+	venipede: {
+		tier: "LC",
+	},
+	whirlipede: {
+		tier: "NFE",
+	},
+	scolipede: {
+		tier: "OU",
+	},
+	cottonee: {
+		tier: "LC",
+	},
+	whimsicott: {
+		tier: "OU",
+	},
+	petilil: {
+		tier: "LC",
+	},
+	lilligant: {
+		tier: "OU",
+	},
+	basculin: {
+		tier: "OU",
+	},
+	sandile: {
+		tier: "LC",
+	},
+	krokorok: {
+		tier: "NFE",
+	},
+	krookodile: {
+		tier: "OU",
+	},
+	darumaka: {
+		tier: "LC",
+	},
+	darmanitan: {
+		tier: "OU",
+	},
+	maractus: {
+		tier: "OU",
+	},
+	dwebble: {
+		tier: "LC",
+	},
+	crustle: {
+		tier: "OU",
+	},
+	scraggy: {
+		tier: "LC",
+	},
+	scrafty: {
+		tier: "OU",
+	},
+	sigilyph: {
+		tier: "OU",
+	},
+	yamask: {
+		tier: "LC",
+	},
+	cofagrigus: {
+		tier: "OU",
+	},
+	tirtouga: {
+		tier: "LC",
+	},
+	carracosta: {
+		tier: "OU",
+	},
+	archen: {
+		tier: "LC",
+	},
+	archeops: {
+		tier: "OU",
+	},
+	trubbish: {
+		tier: "LC",
+	},
+	garbodor: {
+		tier: "OU",
+	},
+	zorua: {
+		tier: "LC",
+	},
+	zoroark: {
+		tier: "OU",
+	},
+	minccino: {
+		tier: "LC",
+	},
+	cinccino: {
+		tier: "OU",
+	},
+	gothita: {
+		tier: "LC",
+	},
+	gothorita: {
+		tier: "NFE",
+	},
+	gothitelle: {
+		tier: "OU",
+	},
+	solosis: {
+		tier: "LC",
+	},
+	duosion: {
+		tier: "NFE",
+	},
+	reuniclus: {
+		tier: "OU",
+	},
+	ducklett: {
+		tier: "LC",
+	},
+	swanna: {
+		tier: "OU",
+	},
+	vanillite: {
+		tier: "LC",
+	},
+	vanillish: {
+		tier: "NFE",
+	},
+	vanilluxe: {
+		tier: "OU",
+	},
+	deerling: {
+		tier: "LC",
+	},
+	sawsbuck: {
+		tier: "OU",
+	},
+	emolga: {
+		tier: "OU",
+	},
+	karrablast: {
+		tier: "LC",
+	},
+	escavalier: {
+		tier: "OU",
+	},
+	foongus: {
+		tier: "LC",
+	},
+	amoonguss: {
+		tier: "OU",
+	},
+	frillish: {
+		tier: "LC",
+	},
+	jellicent: {
+		tier: "OU",
+	},
+	alomomola: {
+		tier: "OU",
+	},
+	joltik: {
+		tier: "LC",
+	},
+	galvantula: {
+		tier: "OU",
+	},
+	ferroseed: {
+		tier: "LC",
+	},
+	ferrothorn: {
+		tier: "OU",
+	},
+	klink: {
+		tier: "LC",
+	},
+	klang: {
+		tier: "NFE",
+	},
+	klinklang: {
+		tier: "OU",
+	},
+	tynamo: {
+		tier: "LC",
+	},
+	eelektrik: {
+		tier: "NFE",
+	},
+	eelektross: {
+		tier: "OU",
+	},
+	elgyem: {
+		tier: "LC",
+	},
+	beheeyem: {
+		tier: "OU",
+	},
+	litwick: {
+		tier: "LC",
+	},
+	lampent: {
+		tier: "NFE",
+	},
+	chandelure: {
+		tier: "OU",
+	},
+	axew: {
+		tier: "LC",
+	},
+	fraxure: {
+		tier: "NFE",
+	},
+	haxorus: {
+		tier: "OU",
+	},
+	cubchoo: {
+		tier: "LC",
+	},
+	beartic: {
+		tier: "OU",
+	},
+	cryogonal: {
+		tier: "OU",
+	},
+	shelmet: {
+		tier: "LC",
+	},
+	accelgor: {
+		tier: "OU",
+	},
+	stunfisk: {
+		tier: "OU",
+	},
+	mienfoo: {
+		tier: "LC",
+	},
+	mienshao: {
+		tier: "OU",
+	},
+	druddigon: {
+		tier: "OU",
+	},
+	golett: {
+		tier: "LC",
+	},
+	golurk: {
+		tier: "OU",
+	},
+	pawniard: {
+		tier: "LC",
+	},
+	bisharp: {
+		tier: "OU",
+	},
+	bouffalant: {
+		tier: "OU",
+	},
+	rufflet: {
+		tier: "LC",
+	},
+	braviary: {
+		tier: "OU",
+	},
+	vullaby: {
+		tier: "LC",
+	},
+	mandibuzz: {
+		tier: "OU",
+	},
+	heatmor: {
+		tier: "OU",
+	},
+	durant: {
+		tier: "OU",
+	},
+	deino: {
+		tier: "LC",
+	},
+	zweilous: {
+		tier: "NFE",
+	},
+	hydreigon: {
+		tier: "OU",
+	},
+	larvesta: {
+		tier: "LC",
+	},
+	volcarona: {
+		tier: "OU",
+	},
+	cobalion: {
+		tier: "OU",
+	},
+	terrakion: {
+		tier: "OU",
+	},
+	virizion: {
+		tier: "OU",
+	},
+	tornadus: {
+		tier: "OU",
+	},
+	thundurus: {
+		tier: "OU",
+	},
+	reshiram: {
+		tier: "Uber",
+	},
+	zekrom: {
+		tier: "Uber",
+	},
+	landorus: {
+		tier: "Uber",
+	},
+	kyurem: {
+		tier: "OU",
+	},
+	kyuremblack: {
+		tier: "OU",
+	},
+	kyuremwhite: {
+		tier: "Uber",
+	},
+	tornadustherian: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	thundurustherian: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	landorustherian: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	kyuremblack: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	kyuremwhite: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	keldeo: {
+		isNonstandard: "Unobtainable",
+		tier: "Unreleased",
+	},
+	keldeoresolute: {
+		isNonstandard: "Unobtainable",
+		tier: "Unreleased",
+	},
+	meloetta: {
+		isNonstandard: "Unobtainable",
+		tier: "Unreleased",
+	},
+	genesect: {
+		isNonstandard: "Unobtainable",
+		tier: "Unreleased",
+	},
