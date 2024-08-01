1262ed6669151e970c550d9b607dde557350cbbb
tisdylannx <itsdylannx@gmail.com>
2024-07-31 20:59:15 -0400

    Create scripts.ts

---
+++ data/mods/sillymons/scripts.ts
@@ -0,0 +1,9 @@
+export const Scripts: ModdedBattleScriptsData = {
+	init() {
+		this.modData('Learnsets', 'ursaluna').learnset.uturn = ["9L1"];
+		this.modData('Learnsets', 'ursaluna').learnset.suckerpunch = ["9L1"];
+		this.modData("Learnsets", "tyranitar").learnset.suckerpunch = ["9L1"];
+		this.modData("Learnsets", "serperior").learnset.dragondance = ["9L1"];
+		this.modData("Learnsets", "entei").learnset.swordsdance = ["9L1"];
+	},
+};
