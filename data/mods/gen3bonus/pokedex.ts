a0fdcbee8b7c24eb868030ca24274297c0b9009b
tisdylannx <itsdylannx@gmail.com>
2024-08-02 16:05:21 -0400

    Update pokedex.ts

--- data/mods/gen3bonus/pokedex.ts
+++ data/mods/gen3bonus/pokedex.ts
@@ -2,363 +2,453 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 	ogerpon: {
 		inherit: true,
 		abilities: {0: "Chlorophyll"},
+		gen: 3,
 	},
 	ogerponcornerstone: {
 		inherit: true,
+		gen: 3,
 	},
 	ogerponhearthflame: {
 		inherit: true,
 		abilities: {0: "Flash Fire"},
+		gen: 3,
 	},
 	ogerponwellspring: {
 		inherit: true,
+		gen: 3,
 	},
 	solosis: {
 		inherit: true,
 		abilities: {0: "Magic Guard", 1: "Own Tempo"},
+		gen: 3,
 	},
 	duosion: {
 		inherit: true,
 		abilities: {0: "Magic Guard", 1: "Own Tempo"},
+		gen: 3,
 	},
 	reuniclus: {
 		inherit: true,
 		abilities: {0: "Magic Guard", 1: "Own Tempo"},
+		gen: 3,
 	},
 	bergmite: {
 		inherit: true,
 		abilities: {0: "Regenerator", 1: "Intimidate"},
+		gen: 3,
 	},
 	avalugg: {
 		inherit: true,
 		abilities: {0: "Regenerator", 1: "Intimidate"},
+		gen: 3,
 	},
 	rattataalola: {
 		inherit: true,
 		abilities: {0: "Thick Fat", 1: "Rock Head"},
+		gen: 3,
 	},
 	raticatealola: {
 		inherit: true,
 		abilities: {0: "Thick Fat", 1: "Rock Head"},
+		gen: 3,
 	},
 	sandshrewalola: {
 		inherit: true,
 		abilities: {0: "Thick Fat", 1: "Shell Armor"},
+		gen: 3,
 	},
 	sandslashalola: {
 		inherit: true,
 		abilities: {0: "Thick Fat", 1: "Shell Armor"},
+		gen: 3,
 	},
 	vulpixalola: {
 		inherit: true,
 		abilities: {0: "Snow Warning", 1: "Vital Spirit"},
+		gen: 3,
 	},
 	ninetalesalola: {
 		inherit: true,
 		abilities: {0: "Snow Warning", 1: "Vital Spirit"},
+		gen: 3,
 	},
 	diglettalola: {
 		inherit: true,
 		abilities: {0: "Sand Veil", 1: "Arena Trap"},
+		gen: 3,
 	},
 	dugtrioalola: {
 		inherit: true,
 		abilities: {0: "Sand Veil", 1: "Arena Trap"},
+		gen: 3,
 	},
 	meowthalola: {
 		inherit: true,
 		abilities: {0: "Pickup", 1: "Technician"},
+		gen: 3,
 	},
 	persianalola: {
 		inherit: true,
 		abilities: {0: "Fur Coat", 1: "Technician"},
+		gen: 3,
 	},
 	geodudealola: {
 		inherit: true,
 		abilities: {0: "Magnet Pull", 1: "Volt Absorb"},
+		gen: 3,
 	},
 	graveleralola: {
 		inherit: true,
 		abilities: {0: "Magnet Pull", 1: "Volt Absorb"},
+		gen: 3,
 	},
 	golemalola: {
 		inherit: true,
 		abilities: {0: "Magnet Pull", 1: "Volt Absorb"},
+		gen: 3,
 	},
 	grimeralola: {
 		inherit: true,
 		abilities: {0: "Sticky Hold", 1: "Stench"},
+		gen: 3,
 	},
 	mukalola: {
 		inherit: true,
 		abilities: {0: "Sticky Hold", 1: "Stench"},
+		gen: 3,
 	},
 	exeggutoralola: {
 		inherit: true,
 		abilities: {0: "Keen Eye", 1: "Chlorophyll"},
+		gen: 3,
 	},
 	marowakalola: {
 		inherit: true,
 		abilities: {0: "Flame Body", 1: "Rock Head"},
+		gen: 3,
 	},
 	meowthgalar: {
 		inherit: true,
 		abilities: {0: "Tough Claws"},
+		gen: 3,
 	},
 	perrserker: {
 		inherit: true,
 		abilities: {0: "Tough Claws"},
+		gen: 3,
 	},
 	ponytagalar: {
 		inherit: true,
 		types: ["Psychic"],
 		abilities: {0: "Natural Cure"},
+		gen: 3,
 	},
 	rapidashgalar: {
 		inherit: true,
 		types: ["Psychic"],
 		abilities: {0: "Natural Cure"},
+		gen: 3,
 	},
 	farfetchdgalar: {
 		inherit: true,
 		types: ["Fighting", "Flying"],
 		abilities: {0: "Super Luck"},
+		gen: 3,
 	},
 	sirfetchd: {
 		inherit: true,
 		types: ["Fighting", "Flying"],
 		baseStats: {hp: 62, atk: 115, def: 95, spa: 68, spd: 82, spe: 85},
 		abilities: {0: "Super Luck"},
+		gen: 3,
 	},
 	weezinggalar: {
 		inherit: true,
 		types: ["Poison", "Psychic"],
 		abilities: {0: "Levitate"},
+		gen: 3,
 	},
 	mrmimegalar: {
 		inherit: true,
 		abilities: {0: "Magic Guard"},
+		gen: 3,
 	},
 	mrrime: {
 		inherit: true,
 		abilities: {0: "Magic Guard"},
+		gen: 3,
 	},
 	articunogalar: {
 		inherit: true,
 		abilities: {0: "Pressure"},
+		gen: 3,
 	},
 	zapdosgalar: {
 		inherit: true,
 		abilities: {0: "Pressure"},
+		gen: 3,
 	},
 	moltresgalar: {
 		inherit: true,
 		abilities: {0: "Pressure"},
+		gen: 3,
 	},
 	corsolagalar: {
 		inherit: true,
 		abilities: {0: "Regenerator", 1: "Magic Guard"},
+		gen: 3,
 	},
 	cursola: {
 		inherit: true,
 		abilities: {0: "Regenerator", 1: "Magic Guard"},
+		gen: 3,
 	},
 	zigzagoongalar: {
 		inherit: true,
 		abilities: {0: "Run Away", 1: "Intimidate"},
+		gen: 3,
 	},
 	linoonegalar: {
 		inherit: true,
 		abilities: {0: "Run Away", 1: "Intimidate"},
+		gen: 3,
 	},
 	obstagoon: {
 		inherit: true,
 		abilities: {0: "Run Away", 1: "Intimidate"},
+		gen: 3,
 	},
 	darumakagalar: {
 		inherit: true,
+		gen: 3,
 	},
 	darmanitangalar: {
 		inherit: true,
 		abilities: {0: "Hustle", 1: "Inner Focus"},
+		gen: 3,
 	},
 	darumaka: {
 		inherit: true,
 		abilities: {0: "Hustle"},
+		gen: 3,
 	},
 	darmanitan: {
 		inherit: true,
 		abilities: {0: "Sheer Force"},
+		gen: 3,
 	},
 	yamask: {
 		inherit: true,
 		abilities: {0: "Mummy"},
+		gen: 3,
 	},
 	cofagrigus: {
 		inherit: true,
 		abilities: {0: "Mummy"},
+		gen: 3,
 	},
 	yamaskgalar: {
 		inherit: true,
+		gen: 3,
 	},
 	stunfisk: {
 		inherit: true,
 		abilities: {0: "Static", 1: "Limber"},
+		gen: 3,
 	},
 	stunfiskgalar: {
 		inherit: true,
 		abilities: {0: "Limber"},
+		gen: 3,
 	},
 	growlithehisui: {
 		inherit: true,
 		abilities: {0: "Intimidate", 1: "Flash Fire"},
+		gen: 3,
 	},
 	arcaninehisui: {
 		inherit: true,
 		abilities: {0: "Intimidate", 1: "Flash Fire"},
+		gen: 3,
 	},
 	voltorbhisui: {
 		inherit: true,
 		abilities: {0: "Volt Absorb", 1: "Magic Guard"},
+		gen: 3,
 	},
 	electrodehisui: {
 		inherit: true,
 		abilities: {0: "Volt Absorb", 1: "Magic Guard"},
+		gen: 3,
 	},
 	typhlosionhisui: {
 		inherit: true,
 		abilities: {0: "Blaze", 1: "Flash Fire"},
+		gen: 3,
 	},
 	qwilfishhisui: {
 		inherit: true,
 		abilities: {0: "Swift Swim", 1: "Poison Point"},
+		gen: 3,
 	},
 	overqwil: {
 		inherit: true,
 		abilities: {0: "Swift Swim", 1: "Poison Point"},
+		gen: 3,
 	},
 	sneaselhisui: {
 		inherit: true,
+		gen: 3,
 	},
 	sneasler: {
 		inherit: true,
 		abilities: {0: "Speed Boost"},
+		gen: 3,
 	},
 	weavile: {
 		inherit: true,
 		abilities: {0: "Pressure", 1: "Intimidate"},
+		gen: 3,
 	},
 	samurott: {
 		inherit: true,
 		abilities: {0: "Torrent"},
+		gen: 3,
 	},
 	samurotthisui: {
 		inherit: true,
 		abilities: {0: "Torrent"},
+		gen: 3,
 	},
 	dewott: {
 		inherit: true,
 		abilities: {0: "Torrent"},
+		gen: 3,
 	},
 	oshawott: {
 		inherit: true,
 		abilities: {0: "Torrent"},
+		gen: 3,
 	},
 	lilligant: {
 		inherit: true,
 		abilities: {0: "Chlorophyll"},
+		gen: 3,
 	},
 	zoruahisui: {
 		inherit: true,
 		abilities: {0: "Illusion"},
+		gen: 3,
 	},
 	zoroarkhisui: {
 		inherit: true,
 		abilities: {0: "Illusion"},
+		gen: 3,
 	},
 	rufflet: {
 		inherit: true,
 		abilities: {0: "Keen Eye", 1: "Sheer Force"},
+		gen: 3,
 	},
 	braviary: {
 		inherit: true,
 		abilities: {0: "Keen Eye", 1: "Sheer Force"},
+		gen: 3,
 	},
 	braviaryshisui: {
 		inherit: true,	
 		abilities: {0: "Keen Eye", 1: "Sheer Force"},
+		gen: 3,
 	},
 	goomy: {
 		inherit: true,
 		abilities: {0: "Regenerator"},
+		gen: 3,
 	},
 	sliggoo: {
 		inherit: true,
 		abilities: {0: "Regenerator"},
+		gen: 3,
 	},
 	goodra: {
 		inherit: true,
 		abilities: {0: "Regenerator"},
+		gen: 3,
 	},
 
 	sliggoohisui: {
 		inherit: true,
 		abilities: {0: "Regenerator"},
+		gen: 3,
 	},
 	goodrahisui: {
 		inherit: true,
 		abilities: {0: "Regenerator"},
+		gen: 3,
 	},
 	avalugghisui: {
 		inherit: true,
 		abilities: {0: "Rock Head", 1: "Sheer Force"},
+		gen: 3,
 	},
 	rowlet: {
 		inherit: true,
 		abilities: {0: "Overgrow"},
+		gen: 3,
 	},
 	dartrix: {
 		inherit: true,
 		abilities: {0: "Overgrow"},
+		gen: 3,
 	},
 	decidueye: {
 		inherit: true,
 		abilities: {0: "Overgrow"},
+		gen: 3,
 	},
 	decidueyehisui: {
 		inherit: true,
 		abilities: {0: "Overgrow"},
+		gen: 3,
 	},
 	wooperpaldea: {
 		inherit: true,
 		abilities: {0: "Water Absorb", 1: "Regenerator"},
+		gen: 3,
 	},
 	clodsire: {
 		inherit: true,
 		abilities: {0: "Water Absorb", 1: "Regenerator"},
+		gen: 3,
 	},
 	taurospaldeacombat: {
 		inherit: true,
 		abilities: {0: "Sheer Force", 1: "Intimidate"},
+		gen: 3,
 	},
 	taurospaldeablaze: {
 		inherit: true,
 		abilities: {0: "Sheer Force", 1: "Flash Fire"},
+		gen: 3,
 	},
 	taurospaldeaaqua: {
 		inherit: true,
 		abilities: {0: "Sheer Force", 1: "Water Absorb"},
+		gen: 3,
 	},
 	slowpokegalar: {
 		inherit: true,
 		abilities: {0: "Poison Point"},
+		gen: 3,
 	},
 	slowbrogalar: {
 		inherit: true,
 		abilities: {0: "Poison Point"},
+		gen: 3,
 	},
 	slowkinggalar: {
 		inherit: true,
 		abilities: {0: "Poison Point"},
+		gen: 3,
 	},
 };
