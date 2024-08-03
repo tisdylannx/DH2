f3e9efaaf49d205a4d84586b8da9db121b358db6
tisdylannx <itsdylannx@gmail.com>
2024-08-03 00:57:23 -0400

    Create items.ts

---
+++ data/mods/gen3bonus/items.ts
@@ -0,0 +1,14 @@
+export const Items: {[k: string]: ModdedItemData} = {
+	cornerstonemask: {
+		inherit: true,
+		gen: 3,
+	},
+	hearthflamemask: {
+		inherit: true,
+		gen: 3,
+	},
+	wellspringmask: {
+		inherit: true,
+		gen: 3,
+	},
+};
