2d6b0fd7471e6a0fd6131ccf4f310a1f28eb4585
tisdylannx <itsdylannx@gmail.com>
2024-08-03 00:57:43 -0400

    Update moves.ts

--- data/mods/gen3bonus/moves.ts
+++ data/mods/gen3bonus/moves.ts
@@ -1,8 +1,18 @@
 export const Moves: {[k: string]: ModdedMoveData} = {
 	ivycudgel: {
 		inherit: true,
+		gen: 3,
 	},
 	chloroblast: {
 		inherit: true,
+		gen: 3,
+	},
+	victorydance: {
+		inherit: true,
+		gen: 3,
+	},
+	gigatonhammer: {
+		inherit: true,
+		gen: 3,
 	},
 };
