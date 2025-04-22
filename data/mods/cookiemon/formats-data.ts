d3a00ffe65ff4c1c7797f39a86d5c4ba8299d704
tisdylannx <itsdylannx@gmail.com>
2025-04-21 21:36:01 -0400

    cookiemon formats data (WIP)

---
+++ data/mods/cookiemon/formats-data.ts
@@ -0,0 +1,44 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	shadowmilkcookie: {
+		tier: "OU",
+	},
+	mysticflowercookie: {
+		tier: "OU",
+	},
+	burningspicecookie: {
+		tier: "OU",
+	},
+	windarchercookie: {
+		tier: "OU",
+	},
+	firespiritcookie: {
+		tier: "OU",
+	},
+	seafairycookie: {
+		tier: "OU",
+	},
+	cottoncookie: {
+		tier: "OU",
+	},
+	goldenosmanthuscookie: {
+		tier: "OU",
+	},
+	rebelcookie: {
+		tier: "OU",
+	},
+	darkcacaocookie: {
+		tier: "OU",
+	},
+	goldencheesecookie: {
+		tier: "OU",
+	},
+	hollyberrycookie: {
+		tier: "OU",
+	},
+	whitelilycookie: {
+		tier: "OU",
+	},
+	purevanillacookie: {
+		tier: "OU",
+	},
+};
